86f3710452b4f87d8ebfe515faeb7cc785a33630
sushilnamberdar <sushilnamberdar000@gmail.com>
2024-09-08 14:03:53 +0530

    update

--- src/Component/Result.js
+++ src/Component/Result.js
@@ -6,31 +6,29 @@ import { toast, ToastContainer } from 'react-toastify';
 import { url } from './uri';
 import { FaLinkedin } from 'react-icons/fa';
 
-const Result = ({ correctAnswersCount, totalQuestions, uid, uname,userAnswers }) => {
+const Result = ({ correctAnswersCount, totalQuestions, uid, uname, userAnswers }) => {
   const navigate = useNavigate();
   const percentage = (correctAnswersCount / totalQuestions) * 100;
   const testDate = new Date().toLocaleDateString();
 
   const attemptedQuestions = userAnswers.filter(answer => answer.status === 'attempted');
 
-
   const test_result = {
     id: uid,
     totalQuestions,
     correctAnswersCount,
     percentage,
-    attemptedQuestions: attemptedQuestions.map(({ status,answer, correctAnswer, Questionid }) => ({
+    attemptedQuestions: attemptedQuestions.map(({ status, answer, correctAnswer, Questionid }) => ({
       status,
       answer,
       correctAnswer,
       Questionid
     })),
   };
- 
+
   useEffect(() => {
     axios.post(`${url}/testresult`, test_result)
       .then((response) => {
-       
         toast.success('Results submitted successfully!');
       })
       .catch((error) => {
@@ -41,6 +39,7 @@ const Result = ({ correctAnswersCount, totalQuestions, uid, uname,userAnswers })
         }
       });
   }, []);
+
   const Name = uname.name.toUpperCase();
 
   const handleRetakeQuiz = () => {
@@ -48,52 +47,49 @@ const Result = ({ correctAnswersCount, totalQuestions, uid, uname,userAnswers })
   };
 
   return (
-    <div className='w-full'>
-      <div className=' absolute'>
-      <Link to={'/'}>
-      <img className='h-10 w-10 ml-3 mt-3 '  src='./logo.png'/>
-      </Link> 
+    <div className='w-full min-h-screen flex flex-col justify-between'>
+      <div className='absolute'>
+        <Link to={'/'}>
+          <img className='h-10 w-10 ml-3 mt-3' src='./logo.png' alt='Logo' />
+        </Link>
       </div>
-    <div className='flex flex-col items-center justify-center w-full  min-h-screen bg-gradient-to-r from-green-400 via-blue-500 to-purple-600'>
-      
-      <div className='w-[90%] max-w-3xl mt-40 lg:mt-1 mb-10 p-10 bg-white shadow-xl rounded-lg text-center'>
-        <h1 className='text-4xl font-bold mb-5'>Certificate of Achievement</h1>
-        <p className='text-xl mb-5'>Date: {testDate}</p>
-        <p className='text-2xl mb-5'>This certifies that</p>
-        <h2 className='text-3xl font-bold mb-5'>{Name}</h2>
-
-        <p className='text-xl mb-5'>has successfully completed the MCQ test with the following results:</p>
-        <p className='text-xl mb-5'><span className=' font-bold'>Email:</span> {uname.email}</p>
-        <p className='text-xl mb-5'><span className=' font-bold'>Phone:</span> {uname.mobileno}</p>
-        <p className='text-xl mb-5'> <span className=' font-bold'>Number of Questions:</span> {totalQuestions}</p>
-        <div className='bg-slate-200 mt-10 p-5 rounded-lg  '>
-        <p className='text-xl font-bold mb-5'>Got {correctAnswersCount} Marks</p>
-        <p className='text-2xl  font-light mb-8'>{percentage.toFixed(2)}%</p>
+      <div className='flex flex-col items-center justify-center w-full grow bg-gradient-to-r from-green-400 via-blue-500 to-purple-600'>
+        <div className='w-[90%] max-w-3xl mt-40 lg:mt-1 mb-10 p-10 bg-white shadow-xl rounded-lg text-center'>
+          <h1 className='text-4xl font-bold mb-5'>Certificate of Achievement</h1>
+          <p className='text-xl mb-5'>Date: {testDate}</p>
+          <p className='text-2xl mb-5'>This certifies that</p>
+          <h2 className='text-3xl font-bold mb-5'>{Name}</h2>
+          <p className='text-xl mb-5'>has successfully completed the MCQ test with the following results:</p>
+          <p className='text-xl mb-5'><span className=' font-bold'>Email:</span> {uname.email}</p>
+          <p className='text-xl mb-5'><span className=' font-bold'>Phone:</span> {uname.mobileno}</p>
+          <p className='text-xl mb-5'><span className=' font-bold'>Number of Questions:</span> {totalQuestions}</p>
+          <div className='bg-slate-200 mt-10 p-5 rounded-lg'>
+            <p className='text-xl font-bold mb-5'>Got {correctAnswersCount} Marks</p>
+            <p className='text-2xl font-light mb-8'>{percentage.toFixed(2)}%</p>
+          </div>
+          <button
+            className='bg-blue-500 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            onClick={handleRetakeQuiz}
+          >
+            Retake Quiz
+          </button>
         </div>
-        
-        <button
-          className='bg-blue-500 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-          onClick={handleRetakeQuiz}
-        >
-          Retake Quiz
-        </button>
+        <ToastContainer />
       </div>
-      <ToastContainer />
-    </div>
-    <footer className="bg-gray-200 w-full text-center flex items-center fixed mb-0 abs justify-center p-4">
-      <p className="text-gray-600 flex items-center">
-        &copy; {new Date().getFullYear()} Quiz App. All rights reserved.{' '}
-        <a 
-          href="https://www.linkedin.com/in/sushilnamberdar" 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="ml-2 flex items-center"
-        >
-          <FaLinkedin className="text-blue-700 mr-1" />
-          LinkedIn
-        </a>
-      </p>
-    </footer>
+      <footer className="bg-gray-200 w-full text-center flex items-center fixed bottom-0 justify-center p-4">
+        <p className="text-gray-600 flex items-center">
+          &copy; {new Date().getFullYear()} Quiz App. All rights reserved.{' '}
+          <a 
+            href="https://www.linkedin.com/in/sushilnamberdar" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="ml-2 flex items-center"
+          >
+            <FaLinkedin className="text-blue-700 mr-1" />
+            LinkedIn
+          </a>
+        </p>
+      </footer>
     </div>
   );
 };
--- src/Component/WelcomePage.js
+++ src/Component/WelcomePage.js
@@ -55,31 +55,26 @@ const WelcomePage = ({ onQuestionsUpdate }) => {
   return (
     <>
       <div className="min-h-screen flex flex-col bg-gray-100">
-        <div className='w-full flex items-center justify-between mt-2 ml-2'>
-          <Link to={'/'} >
-            <img className='h-10 w-10' src='./logo.png' />
+        <header className="w-full flex items-center justify-between mt-2 ml-2">
+          <Link to={'/'}>
+            <img className='h-10 w-10' src='./logo.png' alt="Logo" />
           </Link>
           <div>
             <Link className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors mr-9" to='/admin'>
               Admin Login
             </Link>
           </div>
-        </div>
-        <div className="flex-grow flex flex-col items-center justify-center p-4 relative">
-          <div className="absolute top-4 right-4 sm:top-4 sm:right-4">
+        </header>
 
-
-          </div>
-
-          {/* Conditional rendering for loader or content */}
+        <main className="flex-grow flex flex-col items-center justify-center p-4 relative">
           {loading ? (
             <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-              <div className="animate-pulse flex space-x-4 w-full" >
+              <div className="animate-pulse flex space-x-4 w-full">
                 <div className="rounded-full bg-slate-700 h-10 w-10"></div>
                 <div className="flex-1 space-y-6 py-1">
                   <div className="h-2 bg-slate-700 rounded"></div>
                   <div>Loading...</div>
-                  <img src='./logo.png' />
+                  <img src='./logo.png' alt="Loading logo" />
                   <div className="space-y-3">
                     <div className="grid grid-cols-3 gap-4">
                       <div className="h-2 bg-slate-700 rounded col-span-2"></div>
@@ -92,7 +87,7 @@ const WelcomePage = ({ onQuestionsUpdate }) => {
             </div>
           ) : (
             <>
-              <div className="max-w-2xl w-full text-center sm:top-4 mt-12">
+              <div className="max-w-2xl w-full text-center mt-12">
                 <h1 className="text-4xl font-bold text-blue-600 mb-6">Welcome to the Quiz App</h1>
                 <p className="text-lg text-gray-700 mb-8">Test and improve your knowledge with our quizzes. Choose a category below to get started!</p>
 
@@ -117,7 +112,8 @@ const WelcomePage = ({ onQuestionsUpdate }) => {
               </button>
             </>
           )}
-        </div>
+        </main>
+
         <footer className="bg-gray-200 w-full text-center flex items-center justify-center p-4">
           <p className="text-gray-600 flex items-center">
             &copy; {new Date().getFullYear()} Quiz App. All rights reserved.{' '}
@@ -135,6 +131,6 @@ const WelcomePage = ({ onQuestionsUpdate }) => {
       </div>
     </>
   );
-}
+};
 
 export default WelcomePage;
